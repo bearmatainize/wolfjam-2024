4dfd3003c623ca97e4e5c27df89e2ad31dca7853
Triston Lincoln <79023422+trl6895@users.noreply.github.com>
2024-11-15 21:54:30 -0500

    Created folders and base gridmanager and tile scripts

---
+++ Wolfjam-2024/Assets/Scripts/GridManager.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class GridManager : MonoBehaviour
+{
+
+    [SerializeField] private width, height;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
