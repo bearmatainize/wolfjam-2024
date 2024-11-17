c2cb917c27147b685630451d4dd76a27b9c5c16b
Arica Miller <arica720@hotmail.com>
2024-11-16 22:11:43 -0500

    Test and Next Buttons

--- Wolfjam-2024/Assets/Scripts/TruthTableManager.cs
+++ Wolfjam-2024/Assets/Scripts/TruthTableManager.cs
@@ -7,15 +7,36 @@ public class TruthTableManager : MonoBehaviour
 {
     public TMP_Text yours;
     public TMP_Text goal;
+    public Button test;
+    public Button next;
+    public RectTransform testRectTransform;
+    public RectTransform nextRectTransform;
 
     private string[] goals = new string[] {
-        "0\n0\n0\n0\n"
+        "0\n1\n1\n1\n", //Or
+        "0\n0\n0\n1\n", //And
+        "1\n1\n1\n0\n",
+        "1\n0\n0\n1\n",
+        "1\n0\n0\n0\n"
     };
 
     private void Start()
     {
         goal.text = goals[0];
-        yours.text = "1\n1\n1\n1\n";
+        yours.text = "0\n0\n0\n0\n";
+        test.interactable = true;
+        next.interactable = false;
+        // Get the RectTransform of the buttons
+        nextRectTransform = next.GetComponent<RectTransform>();
+        testRectTransform = test.GetComponent<RectTransform>();
+
+        // Set the Z position of the buttons' RectTransform
+        Vector3 nextNewPosition = nextRectTransform.localPosition;
+        nextNewPosition.y = nextNewPosition.y + 10000f;
+        nextRectTransform.localPosition = nextNewPosition;
+        Vector3 testNewPosition = testRectTransform.localPosition;
+        testNewPosition.y = testNewPosition.y - 10000f;
+        testRectTransform.localPosition = testNewPosition;
     }
 
     public void Check()
@@ -23,6 +44,17 @@ public class TruthTableManager : MonoBehaviour
         if (yours.text == goal.text)
         {
             Debug.Log("Correct!");
+            test.interactable = false;
+            next.interactable = true;
+
+            // Set the Z position of the buttons' RectTransform
+            Vector3 testNewPosition = testRectTransform.localPosition;
+            testNewPosition.y = testNewPosition.y + 10000f;
+            testRectTransform.localPosition = testNewPosition;
+            Vector3 nextNewPosition = nextRectTransform.localPosition;
+            nextNewPosition.y = nextNewPosition.y - 10000f;
+            nextRectTransform.localPosition = nextNewPosition;
+            
         }
         else
         {
