bc32c2d7dc967057f411887ee856706aca37beb7
Triston Lincoln <79023422+trl6895@users.noreply.github.com>
2024-11-16 22:01:02 -0500

    Created New scenes for levels, updated game manager to get gates index

--- Wolfjam-2024/Assets/Scripts/GameManager.cs
+++ Wolfjam-2024/Assets/Scripts/GameManager.cs
@@ -26,12 +26,33 @@ public class GameManager : MonoBehaviour
 
     public List<int> resultsList = new List<int>();
 
+    [SerializeField] int gatesIndex;
+
+    private string[] orLevelGates = new string[] { "or" };                              //0
+    private string[] andLevelGates = new string[] { "and" };                            //1
+    private string[] notLevelGates = new string[] { "not" };                            //2
+    private string[] nandLevelGates = new string[] { "not", "and" };                    //3
+    private string[] pureNandLevelGates = new string[] { "nand" };                      //4
+    private string[] norLevelGates = new string[] { "nor" };                            //5
+    private string[] norFromNandGates = new string[] { "nand", "nand", "nand", "nand" };//6
+
+    private List<string[]> gatesLists = new List<string[]>();
+
     public Camera Cam
     { get { return cam; } }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+
+        gatesLists.Add(orLevelGates);
+        gatesLists.Add(andLevelGates);
+        gatesLists.Add(notLevelGates);
+        gatesLists.Add(nandLevelGates);
+        gatesLists.Add(pureNandLevelGates);
+        gatesLists.Add(norLevelGates);
+        gatesLists.Add(norFromNandGates);
+
         truthTableManager = GameObject.Find("TruthTable").GetComponent<TruthTableManager>();
 
         gridManager.GenerateGrid();
@@ -40,7 +61,7 @@ public class GameManager : MonoBehaviour
         var newTray = Instantiate(trayPrefab);
         newTray.transform.position = new Vector3(cam.transform.position.x - 2.0f, cam.transform.position.y - 4.5f, 0.0f);
 
-        string[] gates = new string[] { "or", "not", "xor", "and", "nor", "nand", "xnor" };
+        string[] gates = gatesLists[gatesIndex];
         newTray.AddGateComponents(gates);
 
         List<Tile> allTiles = gridManager.AllTiles;
